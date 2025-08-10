4ef42636942d5a8301b837199d57ebce07110b8a
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-08-10 14:42:06 -0400

    Implemented quests

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// WebSocket
+
 const socket = new WebSocket("ws://localhost:8000/ws");
 const terminal = document.getElementById("terminal")
 const chat = document.getElementById("chat-message")
@@ -62,4 +64,21 @@ function sanitize(string) {
   };
   const reg = /[&<>"'/]/ig;
   return string.replace(reg, (match)=>(map[match]));
-}
+}// Quests
+
+fetch('quests.json')
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  return response.json();
+})
+.then(quests => {
+    const index = Math.floor(Math.random() * quests.length);
+    const quest = quests[index];
+    document.getElementById('quest-title').innerHTML = `${quest.title}`;
+    document.getElementById('quest-description').innerHTML = `${quest.description}`;
+})
+.catch(error => {
+  console.error('Error fetching quests:', error);
+});
